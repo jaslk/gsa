6bd4fd88fee6190c5ee54f4cb9ce4c563b93790e
Fernanda <your.email@example.com>
2017-11-13 20:04:04 -0300

    creador del constructor setCovering

--- src/SetCovering.java
+++ src/SetCovering.java
@@ -2,7 +2,12 @@ public class SetCovering {
 
     private int N; //cantidad de columnas de la matriz binaria
     private int M; //cantidad de filas de la matriz binaria
-    private int matriz[][] = new int[M][N]; //matriz binaria de dimensiones MxN
-    private double coste[] = new double [N]; //vector de N dimensiones que representa el costo de una columna j
+    private int matriz[][];//matriz binaria de dimensiones MxN
+    private int coste[]; //vector de N dimensiones que representa el costo de una columna j
 
+
+    public SetCovering(int N, int M){
+        matriz = new int[N][M];
+        coste = new int[N];
+    }
 }
