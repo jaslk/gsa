a414fe6f988a75a61b662de94e691a65e627c22d
Fernanda <your.email@example.com>
2017-11-13 19:45:03 -0300

    se creó una nueva clase llamada SetCovering

---
+++ src/SetCovering.java
@@ -0,0 +1,8 @@
+public class SetCovering {
+
+    private int N; //cantidad de columnas de la matriz binaria
+    private int M; //cantidad de filas de la matriz binaria
+    private int matriz[][] = new int[M][N]; //matriz binaria de dimensiones MxN
+    private double coste[] = new double [N]; //vector de N dimensiones que representa el costo de una columna j
+
+}
