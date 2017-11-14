2a6753f30f8c2ef6c1508b8095c3b78f450d8376
Fernanda <your.email@example.com>
2017-11-14 00:57:37 -0300

    nada cambio

--- src/Agent.java
+++ src/Agent.java
@@ -18,18 +18,28 @@ public class Agent {
 
     }
 
-   public double calculateForce(){
-        force =
+   public double calculateForce(int iteration, int totalAgents){
+        double totalForce=0;
 
+        for(int i=0; i<totalAgents; i++){
+            if (i!=iteration) {
+
+            }
+        }
+
+        return totalForce;
    }
 
-    public double calculateAceleration(){
-      return aceleration = (force / inertialMass);
+
+    public double calculateMass(int iteration){
+        this.inertialMass =
+
     }
 
-    public double calculateMass(){
 
-
+    //FALTA MODIFICARLA PARA QUE SEA PARA UNA ITERACIÓN EN ESPECÍFICO
+    public double calculateAceleration(int iteration, int totalAgents){
+        return this.aceleration = (calculateForce(iteration, totalAgents) / calculateMass(iteration));
     }
 
 }
