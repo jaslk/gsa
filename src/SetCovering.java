a4d39e6c697db1b9d9fa8b0a8c3d55ededab08d9
astersnake <astersnake@gmail.com>
2017-11-15 15:09:44 -0300

    singleton de setcovering y lectura de restricciones y costos agregados

--- src/SetCovering.java
+++ src/SetCovering.java
@@ -4,41 +4,85 @@ import java.util.Vector;
 
 public class SetCovering {
 
+    //Singleton
+    private static SetCovering instance;
+
+    private SetCovering() {
+        try {
+            loadCost();
+            loadMatrix();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized SetCovering getInstance() {
+        if (instance == null) {
+            instance = new SetCovering();
+        }
+        return instance;
+    }
+
     private int N; //cantidad de columnas de la matriz binaria
     private int M; //cantidad de filas de la matriz binaria
-    private int matriz[][];//matriz binaria de dimensiones MxN
-    private int cost[]; //vector de N dimensiones que representa el costo de una columna j
 
 
-    public SetCovering(int M, int N) {
-        matriz = new int[M][N];
-        cost = new int[N];
-    }
+    private Vector<Vector<Integer>> matrix = new Vector<>();//matriz binaria de dimensiones MxN
+    private Vector<Float> costs = new Vector<>(); //vector de N dimensiones que representa el costo de una columna j
+
 
     public double calculateCost(int i) {
         double totalCost = 0;
         for (int j = 0; j < N; j++) {
-            totalCost = totalCost + this.cost[j] * this.matriz[i][j];
+            totalCost = totalCost + this.costs.get(i) * this.matrix.get(i).get(j);
         }
         return totalCost;
     }
 
-    public static Vector<Vector<Float>> loadMatrix() throws Exception {
+    private void loadMatrix() throws Exception {
         String line = null;
         BufferedReader reader = new BufferedReader(new FileReader("test.txt"));
-        Vector<Vector<Float>> matrix = new Vector<>();
         //Se lee linea por linea del archivo hasta llegar a nill
         while ((line = reader.readLine()) != null) {
             //Se crea un nuevo vector para almacenar los valores de la linea
-            Vector<Float> rowVector = new Vector<>();
+            Vector<Integer> rowVector = new Vector<>();
             for (String value : line.split(",")) { // Se separa por comas
-                rowVector.add(Float.valueOf(value));
+                rowVector.add(Integer.valueOf(value));
             }
-            matrix.add(rowVector);
+            this.matrix.add(rowVector);
         }
         reader.close();
-
-        System.out.println("Matrix : " + matrix);
-        return matrix;
     }
+
+    private void loadCost() throws Exception {
+        String line = null;
+        BufferedReader reader = new BufferedReader(new FileReader("costos.txt"));
+        while ((line = reader.readLine()) != null) {
+            for (String value : line.split(",")) { // Se separa por comas
+                costs.add(Float.valueOf(value));
+            }
+        }
+    }
+
+    public int getContrain(int i, int j) {
+        return matrix.get(i).get(j);
+    }
+
+    public Float getCost(int i) {
+        return costs.get(i);
+    }
+
+    public int getContrainsRowSize() {
+        return matrix.size();
+    }
+
+    public int getContrainsColumnSize() {
+        return matrix.firstElement().size();
+    }
+
+    public int getCostSize() {
+        return costs.size();
+    }
+
+
 }
